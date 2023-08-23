83735638b6d7f31328cf1d1d7a1a77269a941dc4
pta tien <anhtien372002@gmail.com>
2023-08-23 22:33:05 +0700

    combine input form form

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from starlette.requests import Request
 
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 @app.get('/', response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
 @app.post("/predict_datapoint")
-async def predict_datapoint(request: Request,
-                              gender: str = Form(...),
-                              ethnicity: str = Form(...),
-                              parental_level_of_education : str = Form(...),
-                              lunch: str = Form(...),
-                              test_preparation_course: str = Form(...),
-                              writing_score: int = Form(...),
-                              reading_score: int = Form(...)):
+async def predict_datapoint(request: Request):
+    form_data = await request.form()
+
     custom_data = CustomData(
-        gender,
-        ethnicity,
-        parental_level_of_education,
-        lunch,
-        test_preparation_course,
-        writing_score,
-        reading_score)
+        form_data['gender'],
+        form_data['ethnicity'],
+        form_data['parental_level_of_education'],
+        form_data['lunch'],
+        form_data['test_preparation_course'],
+        form_data['writing_score'],
+        form_data['reading_score'])
 
     data = custom_data.get_data_as_data_frame()
 
@@ -34,3 +35,4 @@ async def predict_datapoint(request: Request,
     result = predict_pipeline.predict(data)[0]
 
     return templates.TemplateResponse("index.html", {"request": request, "result": result})
+
